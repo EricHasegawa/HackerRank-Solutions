fa797aead5716e0ab27e6e01a5cdf5adc5bc7494
Eric Hasegawa <eric.hasegawa@mail.utoronto.ca>
2020-08-26 12:54:36 -0400

    Update ArrayManipulations.py

--- InterviewPreparationKit/Arrays/ArrayManipulations.py
+++ InterviewPreparationKit/Arrays/ArrayManipulations.py
@@ -1,24 +1,27 @@
 #!/bin/python3
-# IN PROGRESS (Will be finished soon)
 
 import math
 import os
 import random
 import re
 import sys
+import itertools as it
 
 # Complete the arrayManipulation function below.
 def arrayManipulation(n, queries):
-    arr = [0]*n
-    print(queries)
-    for sublist in queries:
-        index1 = sublist[0] - 1
-        index2 = sublist[1]
-        add = sublist[2]
-        for i in range(index1, index2):
-            arr[i] += add  
-        print(arr)
-    return max(arr)
+    # SLOW SOLUTION
+    # arr = [0]*n
+    # for sublist in queries:
+    #     index1 = sublist[0] - 1
+    #     index2 = sublist[1]
+    #     add = sublist[2]
+    #     for i in range(index1, index2):
+    #         arr[i] += add  
+    # return max(arr)
+
+    #FASTER SOLUTION
+    q = it.chain.from_iterable([(a, k), (b, -k)] for a, b, k in queries)
+    return max(it.accumulate(c for _, c in sorted(q, key=lambda x: (x[0], -x[1]))))
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
