c0d355ee1d4a7801c6876dda332e8f47431f4c67
Eric Hasegawa <eric.hasegawa@mail.utoronto.ca>
2020-09-16 16:25:04 -0400

    Update SherlockAndAnagrams.py

--- InterviewPreparationKit/DictionariesAndHashMaps/SherlockAndAnagrams.py
+++ InterviewPreparationKit/DictionariesAndHashMaps/SherlockAndAnagrams.py
@@ -1,5 +1,5 @@
 #!/bin/python3
-
+from collections import Counter
 import math
 import os
 import random
@@ -7,7 +7,22 @@ import re
 import sys
 
 # Complete the sherlockAndAnagrams function below.
+
 def sherlockAndAnagrams(s):
+    count = 0
+    dic = Counter(s)
+    for i in range(2, len(s)):
+        substring = s[0:i]
+        l = len(substring)
+        dic["".join(sorted(substring))] += 1
+        for j in range(1, len(s)):
+            if j + l <= len(s):
+                dic["".join(sorted(s[j:j+l]))] += 1
+    for k, v in dic.items():
+        count += (v * (v - 1)) // 2
+    return count
+
+
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
