9dbace741b46a145f374bd2a3d977376a9d2c4e1
Eric Hasegawa <eric.hasegawa@mail.utoronto.ca>
2020-09-17 10:23:51 -0400

    Update SherlockAndAnagrams.py

--- InterviewPreparationKit/DictionariesAndHashMaps/SherlockAndAnagrams.py
+++ InterviewPreparationKit/DictionariesAndHashMaps/SherlockAndAnagrams.py
@@ -10,14 +10,18 @@ import sys
 
 def sherlockAndAnagrams(s):
     count = 0
+    # Gets count of each character
     dic = Counter(s)
     for i in range(2, len(s)):
         substring = s[0:i]
         l = len(substring)
+        # Sort the substring alphabetically
         dic["".join(sorted(substring))] += 1
+        # Gets other possible substrings of length l
         for j in range(1, len(s)):
             if j + l <= len(s):
                 dic["".join(sorted(s[j:j+l]))] += 1
+    # Formula to get number of anagrams based on number of substrings with matching length and characters
     for k, v in dic.items():
         count += (v * (v - 1)) // 2
     return count
